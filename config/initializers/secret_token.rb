f2d4a978099a9a35c78ececbc34f87f066dcf3ad
assasinbox <assasinbox@gmail.com>
2012-09-08 17:53:42 +0300

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blabla::Application.config.secret_token = '1be0cbea4bc0d6baab494d3270381f508f1942963097792776449e9c4d878347a3f84cd4de908f36da3d4a1ade8f33316ce00168234d3e77e9d27a68d593d68f'
